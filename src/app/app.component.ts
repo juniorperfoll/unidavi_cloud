a3806a6f56a05bc0e7dd93d41ea61d76ba5a2fea
Junior Perfoll <juniorperfoll@Laptop-de-Junior.local>
2024-06-11 21:25:03 -0300

    Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CloudComponent } from './cloud/cloud.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CloudComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'unidavi_cloud';
+  title = 'Unidavi Cloud';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CloudComponent } from './cloud/cloud.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'cloud',
+        component: CloudComponent
+    },
+    {
+        path: '',
+        redirectTo: '/cloud',
+        pathMatch: 'full'
+    }
+];
